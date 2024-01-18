03f1b8485ce10cd261f0d8e30e2bdd4d9bbfeeda
Fiorela <fazahuanchef@gmail.com>
2024-01-18 11:00:59 -0500

    remove axios

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,9 +1,14 @@
-import axios from 'axios';
+//import axios from 'axios';
 
 export class HttpClient {
-    async get(url: string){
-        const { data, status } = await axios.get(url);
-        console.log({status});
-        return { data, status }
+    // async get(url: string){
+    //     const { data, status } = await axios.get(url);
+    //     console.log({status});
+    //     return { data, status }
+    // }
+    async get ( url: string ){
+        const resp = await fetch(url);
+        const data = await resp.json();
+        return { data: '', status: 500}
     }
 }
